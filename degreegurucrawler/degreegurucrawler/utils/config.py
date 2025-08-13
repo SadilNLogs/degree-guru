335f119063f5cb0a492378bafad9993dd6cedfdd
SadilNLogs <120541903+SadilNLogs@users.noreply.github.com>
2025-08-13 08:19:17 +0530

    Update config.py

--- degreegurucrawler/degreegurucrawler/utils/config.py
+++ degreegurucrawler/degreegurucrawler/utils/config.py
@@ -1,12 +1,12 @@
 import os
 import yaml
 
-config_path = "degreegurucrawler/utils/crawler.yaml"
+config_path = "degreegurusadil/utils/crawler.yaml"
 with open(config_path, 'r') as file:
     config = yaml.load(file, Loader=yaml.FullLoader)
 
 embedding_function_config = {
-    "api_key": os.environ.get('OPENAI_API_KEY'),
+    "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": os.environ.get('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'),
     "model_name": config["index"]["openAI_embedding_model"]
 }
 
